fb6f137a3d74986b7c7c6dd81bfbc91d1345128a
ChrisAaren <chrisaaren01@gmail.com>
2025-09-02 13:15:37 -0600

    Add functionality to game while modifying game to accommodate special circumstances

--- logic.js
+++ logic.js
@@ -1,6 +1,40 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+  }
+  if (computerScore > humanScore) {
+    console.log("You lost the game! Try Again.");
+  } else {
+    console.log("You win the game! Congratulations!");
+  }
+}
+
+function playRound(humanChoice, computerChoice) {
+  let human = String(humanChoice);
+
+  if (human === "rock" && computerChoice === "paper") {
+    console.log("You lose! Paper beats Rock!");
+    computerScore++;
+  } else if (human === "paper" && computerChoice === "scissors") {
+    console.log("You lose! Scissors beats Paper!");
+    computerScore++;
+  } else if (human === "scissors" && computerChoice === "rock") {
+    console.log("You lose! Rock beats Scissors!");
+    computerScore++;
+  } else if (human === computerChoice) {
+    console.log(
+      "You and the opponent made the same choice! No points added or lost!",
+    );
+  } else {
+    console.log("You won the round!");
+    console.log("Your opponent chose: ", computerChoice);
+    humanScore++;
+  }
+}
+
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
 
@@ -13,6 +47,8 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice(input) {
-  return input;
+function getHumanChoice() {
+  let choice = prompt("Enter a choice: ");
+
+  return choice.toLowerCase();
 }
