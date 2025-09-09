4368b0e1e34bd4fabe049741a5867a0a442908e2
ChrisAaren <chrisaaren01@gmail.com>
2025-09-09 13:39:41 -0600

    Added seperate method to display the game metrics

--- logic.js
+++ logic.js
@@ -2,80 +2,52 @@
 // Display results/score logic
 // Decide winner logic
 
-let humanScore = 0;
-let computerScore = 0;
-
 function playGame() {
-  const scoreboard = document.querySelector(".scoreboard");
+  let humanScore = 0;
+  let computerScore = 0;
 
-  const rock = document.createElement("button");
-  rock.textContent = "Rock";
-
-  const paper = document.createElement("button");
-  paper.textContent = "paper";
-
-  const scissors = document.createElement("button");
-  scissors.textContent = "scissors";
-
-  scoreboard.appendChild(rock);
-  scoreboard.appendChild(paper);
-  scoreboard.appendChild(scissors);
+  let playerWinFlag = "";
 
   let result = document.createElement("p");
 
   rock.addEventListener("click", () => {
-    playRound("rock", getComputerChoice(), scoreboard);
+    playerWinFlag = playRound("rock", getComputerChoice());
   });
 
   paper.addEventListener("click", () => {
-    playRound("paper", getComputerChoice(), scoreboard);
+    playerWinFlag = playRound("paper", getComputerChoice());
   });
 
   scissors.addEventListener("click", () => {
-    playRound("scissors", getComputerChoice(), scoreboard);
+    playerWinFlag = playRound("scissors", getComputerChoice());
   });
+
+  humanScore += playerWinFlag ? 1 : 0;
+  computerScore += playerWinFlag ? 0 : 1;
 }
-
-function playRound(humanChoice, computerChoice, scoreboard) {
+/*
+ * Returns true if the player wins the round
+ */
+function playRound(humanChoice, computerChoice) {
+  let playerWinFlag = "computer";
   let human = String(humanChoice);
-
-  let results = document.createElement("p");
-  let score = document.createElement("p");
 
   if (human === "rock" && computerChoice === "paper") {
     results.textContent = "You lose! Paper beats Rock!";
-    computerScore++;
   } else if (human === "paper" && computerChoice === "scissors") {
     results.textContent = "You lose! Scissors beats Paper!";
-    computerScore++;
   } else if (human === "scissors" && computerChoice === "rock") {
     results.textContent = "You lose! Rock beats Scissors!";
-    computerScore++;
   } else if (human === computerChoice) {
     results.textContent =
       "You and the opponent made the same choice! No points added or lost!";
+    playerWinFlag = "tie";
   } else {
     results.textContent =
       "You won the round! Your opponent chose: " + computerChoice;
-    humanScore++;
+    playerWinFlag = "human";
   }
-  score.textContent = humanScore;
-  scoreboard.appendChild(score);
-  scoreboard.appendChild(results);
-
-  /*
-  This part is used to check the winner and to keep track of the score.
-  */
-
-  // if (humanScore >= 5) {
-  //   let winner = document.createElement("p");
-  //   winner.textContent = "You win the game! Congradulations!";
-  //   scoreboard.appendChild(winner);
-  // } else {
-  //   let loser = document.createElement("p");
-  //   loser.textContent = "You lost the game! Try Again.";
-  //   scoreboard.appendChild(loser);
-  // }
+  return playerWinFlag;
 }
 
 function getComputerChoice() {
@@ -92,6 +64,28 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let choice = prompt("Enter a choice: ");
-
   return choice.toLowerCase();
+}
+
+function displayGame() {
+  const scoreboard = document.querySelector(".scoreboard");
+
+  const gameTitle = document.createElement("h1");
+  gameTitle.textContent = "Rock-Paper-Scissors";
+
+  const rock = document.createElement("button");
+  rock.textContent = "Rock";
+
+  const paper = document.createElement("button");
+  paper.textContent = "Paper";
+
+  const scissors = document.createElement("button");
+  scissors.textContent = "Scissors";
+
+  scoreboard.appendChild(gameTitle);
+  scoreboard.appendChild(rock);
+  scoreboard.appendChild(paper);
+  scoreboard.appendChild(scissors);
+
+  function displayResults(humanScore, computerScore) {}
 }
